1158efcb71dd1bec5e967dc53d3eb8a6fb4d7ce5
Darcy Glennen <spykr7@gmail.com>
2018-02-19 23:16:12 +1100

    Tweak service worker

--- src/sw.js
+++ src/sw.js
@@ -2,21 +2,24 @@ module.exports = {
     staticFileGlobs: [
         'build/**/*.js',
         'build/**/*.css',
-        'build/static/media/*',
-        'build/index.html',
-        'build/room/index.html',
+        'build/static/media/*.jpg',
+        'build/static/media/*.svg',
+        'build/static/media/*.woff2',
         'build/200.html'
     ],
+    dontCacheBustUrlsMatching: /./,
+    swFilePath: 'build/service-worker.js',
     stripPrefix: 'build/',
-    swFilePath: './build/service-worker.js',
     navigateFallback: '/200.html',
-    navigateFallbackWhitelist: [/^(?!.*[.]zip$).*$/],
     cacheId: 'drawasaurus-cache',
     runtimeCaching: [{
         urlPattern: /^https:\/\/fonts\.googleapis\.com/,
         handler: 'cacheFirst'
     }, {
         urlPattern: /^https:\/\/fonts\.gstatic\.com/,
+        handler: 'cacheFirst'
+    }, {
+        urlPattern: /static/,
         handler: 'cacheFirst'
     }]
 }

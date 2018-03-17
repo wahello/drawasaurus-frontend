6d0ea4b4bb29f5afd16c6daa9de567b608e5c4ed
Darcy Glennen <spykr7@gmail.com>
2018-03-17 20:57:00 +1100

    Implemented Sentry for error logging

--- src/index.js
+++ src/index.js
@@ -8,6 +8,9 @@ import registerServiceWorker from './registerServiceWorker';
 import 'normalize.css';
 import './styles/fa-drawasaurus.css';
 
+import Raven from 'raven-js';
+Raven.config('https://1f25cfbe6e1043adb70d91db713327fe@sentry.io/305501').install();
+
 const rootElement = document.getElementById('root');
 
 if( rootElement.hasChildNodes() ) {
